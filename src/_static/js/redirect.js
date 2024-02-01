d6365cd5d25724e499b44dd71212e9b238b66dbf
Pigeon <pigeon43.stuff@gmail.com>
2024-02-01 22:58:49 +0000

    Add 404 page + redirect support

--- src/_static/js/redirect.js
+++ src/_static/js/redirect.js
@@ -1,1 +1,9 @@
-console.log(document.location.href)
+window.addEventListener("DOMContentLoaded", function () {
+    fetch(window.location.href).then((response) => {
+        if (response.status === 404) {
+            if (window.location.pathname.startsWith("/wiki")) {
+                window.location.replace("https://pigeon.miraheze.org" + window.location.pathname)
+            }
+        }
+    })
+})
