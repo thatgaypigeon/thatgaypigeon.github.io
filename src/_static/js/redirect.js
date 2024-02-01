e85713d3f0bbea1231927f0f80b28ffbe5eca802
Pigeon <pigeon43.stuff@gmail.com>
2024-02-01 23:28:44 +0000

    Update redirect.js

--- src/_static/js/redirect.js
+++ src/_static/js/redirect.js
@@ -1,8 +1,14 @@
 window.addEventListener("DOMContentLoaded", function () {
+    fetch("/_static/config.json")
+        .then((response) => response.json())
+        .then((data) => {
+            WIKI_URL = data.wiki_url
+        })
+
     fetch(window.location.href).then((response) => {
         if (response.status === 404) {
             if (window.location.pathname.startsWith("/wiki")) {
-                window.location.replace("https://pigeon.miraheze.org" + window.location.pathname)
+                window.location.replace(WIKI_URL + window.location.pathname)
             }
         }
     })
